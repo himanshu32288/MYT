4cc441ac35dfc255f29bf2450c07a01e90f4f105
Himanshu <himanshu32288@gmail.com>
2021-12-31 10:51:47 +0530

    schedule overflow

--- Frontend/src/Matches/Schedule.js
+++ Frontend/src/Matches/Schedule.js
@@ -3,9 +3,47 @@ const Shedule = () => {
   return (
     <div className="schedule">
       <div className="schedulebar">
-        <li>RESULT</li>
-        <li>TODAY</li>
-        <li>UPCOMING</li>
+        <div className="results">Results</div>
+        <div className="today">Today</div>
+        <div className="upcoming">Upcoming</div>
+      </div>
+      <div className="matches">
+        <h1>IND VS BANchod</h1>
+        <h1>IND VS BANsss</h1>
+        <h1>IND VS BAN</h1>
+        <h1>IND VS BAN</h1>
+        <h1>IND VS BAN</h1>
+        <h1>IND VS BAN</h1>
+        <h1>IND VS BAN</h1>
+        <h1>IND VS BAN</h1>
+        <h1>IND VS BAN</h1>
+        <h1>IND VS BAN</h1>
+        <h1>IND VS BAN</h1>
+        <h1>IND VS BAN</h1>
+        <h1>IND VS BAN</h1>
+        <h1>IND VS BAN</h1>
+        <h1>IND VS BAN</h1>
+        <h1>IND VS BAN</h1>
+        <h1>IND VS BAN</h1>
+        <h1>IND VS BAN</h1>
+        <h1>IND VS BAN</h1>
+        <h1>IND VS BAN</h1>
+        <h1>IND VS BAN</h1>
+        <h1>IND VS BAN</h1>
+        <h1>IND VS BAN</h1>
+        <h1>IND VS BAN</h1>
+        <h1>IND VS BAN</h1>
+        <h1>IND VS BAN</h1>
+        <h1>IND VS BAN</h1>
+        <h1>IND VS BAN</h1>
+        <h1>IND VS BAN</h1>
+        <h1>IND VS BAN</h1>
+        <h1>IND VS BAN</h1>
+        <h1>IND VS BAN</h1>
+        <h1>IND VS BAN</h1>
+        <h1>IND VS BAN</h1>
+        <h1>IND VS BAN</h1>
+        <h1>IND VS BAN</h1>
       </div>
     </div>
   );
