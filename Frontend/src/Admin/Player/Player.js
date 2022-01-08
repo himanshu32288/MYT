76fbd29404c751079de19db54d4fe15ff2d5325b
Himanshu <himanshu32288@gmail.com>
2022-01-08 23:56:58 +0530

    recent form

--- Frontend/src/Admin/Player/Player.js
+++ Frontend/src/Admin/Player/Player.js
@@ -1,51 +1,81 @@
 import { useState } from "react";
 
 const Player = () => {
-  const [recent, addRecent] = useState([
+  const [recent, setRecent] = useState([
     {
-      team1: "halllo",
+      team1: "",
       team2: "",
+      points: "",
+      date: "",
     },
   ]);
+  const [playerName, setPlayerName] = useState("");
   const addfield = (e) => {
     e.preventDefault();
-    addRecent((prev) => {
-      return [...prev, { team1: "", team2: "", points: "" }];
+    setRecent((prev) => {
+      return [...prev, { team1: "", team2: "", points: "", date: "" }];
     });
   };
   const handeleSubmit = (e) => {
     e.preventDefault();
-    console.log(recent);
+    console.log(playerName, recent);
   };
 
-  const changeHandler = (e) => {
-    console.log(e.target.ok);
+  const changeHandler = (e, index) => {
+    const { name, value } = e.target;
+    let list = [...recent];
+    list[index][name] = value;
+    setRecent(list);
+  };
+  const nameHandler = (e) => {
+    const { value } = e.target;
+    setPlayerName(value);
   };
   return (
-    <form onSubmit={handeleSubmit}>
-      <label htmlFor="name">Name</label>
-      <h2>Recent</h2>
-      {recent.map((val, i) => {
-        return (
-          <div className="recent">
-            <label htmlFor={`team1${i}`}>Team1</label>
-            <input
-              type="text"
-              id={`team1${i}`}
-              name="team1"
-              value={recent[i].team1}
-              onChange={changeHandler}
-            />
-            <label htmlFor={`team2${i}`}>Team2</label>
-            <input
-              type="text"
-              name="team2"
-              id={`team2${i}`}
-              index={i}
-              value={recent[i].team2}
-              onChange={changeHandler}
-            />
-            {/* <label htmlFor={`date${i}`}>Date</label>
+    <>
+      <pre>{playerName}</pre>
+      <pre>{JSON.stringify(recent, undefined, 2)}</pre>
+      <form onSubmit={handeleSubmit}>
+        <label htmlFor="name">Name</label>
+        <input type="text" id="name" val={playerName} onChange={nameHandler} />
+
+        <h2>Recent</h2>
+        {recent.map((val, i) => {
+          return (
+            <div className="recent" key={i}>
+              <label htmlFor={`team1${i}`}>Team1</label>
+              <input
+                type="text"
+                id={`team1${i}`}
+                name="team1"
+                value={val.team1}
+                onChange={(e) => changeHandler(e, i)}
+              />
+              <label htmlFor={`team2${i}`}>Team2</label>
+              <input
+                type="text"
+                name="team2"
+                id={`team2${i}`}
+                value={val.team2}
+                onChange={(e) => changeHandler(e, i)}
+              />
+              <label htmlFor="point">Point</label>
+              <input
+                type="number"
+                name="points"
+                id={`points${i}`}
+                value={val.points}
+                onChange={(e) => changeHandler(e, i)}
+              />
+              <label htmlFor="date">Date</label>
+              <input
+                type="date"
+                name="date"
+                id={`date${i}`}
+                value={val.points}
+                onChange={(e) => changeHandler(e, i)}
+              />
+              {/* <label htmlFor={`date${i}`}>Date</label>
             <input
               type="date"
               name="date"
@@ -53,13 +83,14 @@ const Player = () => {
               index={i}
               value={recent[i].date}
               onChange={changeHandler} */}
-            {/* /> */}
-          </div>
-        );
-      })}
-      <button onClick={addfield}>+</button>
-      <button type="submit">Submit</button>
-    </form>
+              {/* /> */}
+            </div>
+          );
+        })}
+        <button onClick={addfield}>+</button>
+        <button type="submit">Submit</button>
+      </form>
+    </>
   );
 };
 
